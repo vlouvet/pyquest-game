27108e8901ecfb7e09bb3b6f1aba9e7a811bdd6d
vlouvet1 <vlouvet@geoforce.com>
2022-02-22 21:59:19 -0700

    continued migration to Flask

--- math_minigame.py
+++ math_minigame.py
@@ -1,42 +1,41 @@
 import random
 
-correctAnswerCount = 0
-def generateFormula(correctAnswerCount, formulaType="addition"):
-    if formulaType == "addition":
-        x = random.randrange(1, 50)
-        y = random.randrange(1, 50)
-        z = x + y
-        print(f"What is the answer to X ({x})+ Y({y})?")
-        answer_list = [random.randrange(1, 100), random.randrange(1, 100), z]
+def generate_formula(correct_answer_count=0, formula_type="addition"):
+    if formula_type == "addition":
+        var_x = random.randrange(1, 50)
+        var_y = random.randrange(1, 50)
+        var_z = var_x + var_y
+        print(f"What is the answer to X ({var_x})+ Y({var_y})?")
+        answer_list = [random.randrange(1, 100), random.randrange(1, 100), var_z]
         answer_list = random.sample(answer_list, len(answer_list))
         answers = {"a": answer_list[0], "b": answer_list[1], "c": answer_list[2]}
-        userInput = input(f"A:{answers['a']}\nB:{answers['b']}\nC:{answers['c']}\n\n")
-        if userInput == "a":
-            if answers["a"] == z:
-                correctAnswerCount +=1
+        user_input = input(f"A:{answers['a']}\nB:{answers['b']}\nC:{answers['c']}\n\n")
+        if user_input == "a":
+            if answers["a"] == var_z:
+                correct_answer_count +=1
                 print("you chose the right answer!")
-                generateFormula(correctAnswerCount, formulaType="addition")
+                generate_formula(correct_answer_count, formula_type="addition")
             else:
                 print("wrong answer!")
-        elif userInput == "b":
-            if answers["b"] == z:
-                correctAnswerCount +=1
+        elif user_input == "b":
+            if answers["b"] == var_z:
+                correct_answer_count +=1
                 print("you chose the right answer!")
-                generateFormula(correctAnswerCount, formulaType="addition")
+                generate_formula(correct_answer_count, formula_type="addition")
             else:
                 print("wrong answer!")
-        elif userInput == "c":
-            if answers["c"] == z:
-                correctAnswerCount +=1
+        elif user_input == "c":
+            if answers["c"] == var_z:
+                correct_answer_count +=1
                 print("you chose the right answer!")
-                generateFormula(correctAnswerCount, formulaType="addition")
+                generate_formula(correct_answer_count, formula_type="addition")
             else:
                 print("wrong answer!")
-        elif userInput == "exit":
-            print(f"You got {correctAnswerCount} answers correct!")
+        elif user_input == "exit":
+            print(f"You got {correct_answer_count} answers correct!")
             exit()
         else:
             print("incorrect input")
 
 
-generateFormula(correctAnswerCount, "addition")
+generate_formula(0, "addition")
