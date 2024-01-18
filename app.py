180a7bc7f8b589cbc6513fabf7396856a96ef320
vlouvet <louvetvicente@gmail.com>
2024-01-17 18:59:48 -0700

    formatting with black

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ def create_app():
                 # TODO: confirm that this user doesn't already exist in the database
                 # (deduplicate on email)
                 user_exists = False
-                if(not model.user_exists(new_user.username)):
+                if not model.user_exists(new_user.username):
                     model.db.session.add(new_user)
                     model.db.session.commit()
-                
+
                 else:
                     flash("That user is already taken!")
-                    return render_template("playgame.html", form=form)                 
+                    return render_template("playgame.html", form=form)
             else:
                 print("Form did not validate!")
                 print(form.errors)
@@ -54,28 +54,28 @@ def create_app():
         user_profile = model.User.query.get(playerid)
         form = gameforms.CharacterForm(obj=user_profile)
         form.charclass.choices = [
-                (PlayerClass.id, PlayerClass.name)
-                for PlayerClass in model.PlayerClass.query.order_by("name")
-            ]
+            (PlayerClass.id, PlayerClass.name)
+            for PlayerClass in model.PlayerClass.query.order_by("name")
+        ]
         form.charrace.choices = [
-                (PlayerRace.id, PlayerRace.name)
-                for PlayerRace in model.PlayerRace.query.order_by("name")
-            ]    
+            (PlayerRace.id, PlayerRace.name)
+            for PlayerRace in model.PlayerRace.query.order_by("name")
+        ]
         # TODO: pre-populate the form with the data from database
         if request.method == "POST":
             form.populate_obj(user_profile)
             # TODO: move this code into a tile_init() function
             current_tile = model.Tile()
             tile_type_list = [
-            {"name":tile_type.name, "id":tile_type.id}
-            for tile_type in model.TileTypeOption.query.order_by("name")
-        ]
+                {"name": tile_type.name, "id": tile_type.id}
+                for tile_type in model.TileTypeOption.query.order_by("name")
+            ]
             form = gameforms.TileForm()
             tile_type = random.choice(tile_type_list)
-            form.type.data = tile_type['name']
+            form.type.data = tile_type["name"]
             current_tile.user_id = playerid
-            current_tile.type = tile_type['id']
-            user_profile.hitpoints=100
+            current_tile.type = tile_type["id"]
+            user_profile.hitpoints = 100
             model.db.session.add(current_tile)
             model.db.session.commit()
             model.db.session.add(user_profile)
@@ -93,12 +93,17 @@ def create_app():
         char_message = "This is a test message to be displayed when the player first starts the game"
         # player_char.getStats()
         return render_template(
-            "charStart.html", charMessage=char_message, player_char_id=id)
+            "charStart.html", charMessage=char_message, player_char_id=id
+        )
 
     @app.route("/player/<int:player_id>/game/tile/next", methods=["POST", "GET"])
     def generate_tile(player_id):
         user_profile = model.User.query.get(player_id)
-        tile_details = model.Tile.query.filter_by(user_id=player_id).order_by(model.Tile.id.desc()).first()
+        tile_details = (
+            model.Tile.query.filter_by(user_id=player_id)
+            .order_by(model.Tile.id.desc())
+            .first()
+        )
         print(f"Queried tile details: {tile_details.id}")
         if tile_details:
             form = gameforms.TileForm(obj=tile_details)
@@ -107,11 +112,11 @@ def create_app():
         form.tileid = tile_details.id
         print(f"Form TileId: {form.tileid}")
         tile_type_list = [
-            {"name":tile_type.name, "id":tile_type.id}
+            {"name": tile_type.name, "id": tile_type.id}
             for tile_type in model.TileTypeOption.query.order_by("name")
         ]
         print(f"Tile_type_List: {tile_type_list}")
-        form.type.data = random.choice(tile_type_list)['name']
+        form.type.data = random.choice(tile_type_list)["name"]
 
         form.tileaction.choices = [
             (tileaction.id, tileaction.name)
@@ -120,10 +125,10 @@ def create_app():
         # if the request was a POST, generate a new tile then display it using gameTile.html
         if request.method == "POST":
             if not tile_details.action_taken:
-                return {'Error': 'Please action this tile before continuing!'}
+                return {"Error": "Please action this tile before continuing!"}
             # start tile_init() code
             current_tile = model.Tile()
-            current_tile.type = random.choice(tile_type_list)['id']
+            current_tile.type = random.choice(tile_type_list)["id"]
             current_tile.user_id = player_id
             if form.tileaction.data:
                 current_tile.action = form.tileaction.data
@@ -142,12 +147,14 @@ def create_app():
     def execute_tile_action(playerid, tileid):
         tile_record = model.Tile.query.get(tileid)
         tileForm = gameforms.TileForm(obj=tile_record)
-        action_type_ID = tileForm.data.get('tileaction')
+        action_type_ID = tileForm.data.get("tileaction")
         if request.method == "POST":
             # validate actionID, return error message if not valid
             if action_type_ID not in [1, 2, 3, 4]:
                 return {"Error": "Bad action selected"}
-            action_record = model.Action.query.filter_by(tile=tileid, actionverb=action_type_ID).first()
+            action_record = model.Action.query.filter_by(
+                tile=tileid, actionverb=action_type_ID
+            ).first()
             if action_record:
                 print(f"Action_record ID: {action_record.id}")
             if tile_record.action_taken == True:
@@ -166,9 +173,16 @@ def create_app():
             model.db.session.add(player_record)
             model.db.session.commit()
             # handle flee from tile
-            return {'status_code':200, 'data':'success'}
+            return {"status_code": 200, "data": "success"}
         else:
-            return {'status_code':200, 'data': {'tile_action':tile_record.action, 'action_taken':tile_record.action_taken, 'user_id':tile_record.user_id}}
+            return {
+                "status_code": 200,
+                "data": {
+                    "tile_action": tile_record.action,
+                    "action_taken": tile_record.action_taken,
+                    "user_id": tile_record.user_id,
+                },
+            }
 
     @app.route("/player/<int:id>/profile", methods=["GET"])
     def get_user_profile(id):
