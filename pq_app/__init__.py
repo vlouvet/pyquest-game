dbd77cd0014e035269c82728739b60e05d9dd10d
vlouvet <louvetvicente@gmail.com>
2024-01-28 22:05:58 -0700

    remove unused imports from init file

--- pq_app/__init__.py
+++ pq_app/__init__.py
@@ -1,7 +1,3 @@
-import json
-import random
-from flask import Flask, request, render_template, redirect, url_for, flash
-
 from .model import *
 from .userCharacter import *
 from .gameforms import *
