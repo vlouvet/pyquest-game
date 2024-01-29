b1d5992b4f22d4a13cb7868fc648df9c77cd27e9
vlouvet <louvetvicente@gmail.com>
2024-01-28 21:53:40 -0700

    dev checkpoint

--- pq_app/__init__.py
+++ pq_app/__init__.py
@@ -2,9 +2,11 @@ import json
 import random
 from flask import Flask, request, render_template, redirect, url_for, flash
 
-from pq_app import model, userCharacter, gameforms, pqMonsters, gameTile
-
-
+from .model import *
+from .userCharacter import *
+from .gameforms import *
+from .pqMonsters import *
+from .gameTile import *
 
 def create_app():
     # create the extension
