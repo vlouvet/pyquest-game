5607449b2a8f745c774c396e9ccf5634e74a8ab7
vlouvet <louvetvicente@gmail.com>
2024-02-19 09:42:11 -0700

    added basic post tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 #testapp.py
 import pytest
 from pq_app import app as pq
-
+from bs4 import BeautifulSoup
 
 @pytest.fixture()
 def app():
@@ -16,6 +16,18 @@ def client(app):
 def test_get_play(client):
     response = client.get("/")
     assert b"<h1>PyQuest Game</h1>" in response.data
+
+def test_post_play(client):
+    # Get the CSRF token
+    response = client.get("/")
+    soup = BeautifulSoup(response.data, 'html.parser')
+    csrf_token = soup.find(id='csrf_token')['value']
+    # Include the CSRF token in the POST request
+    response = client.post("/", data={"username": "l2@v1.c", "csrf_token": csrf_token})
+    postSoup = BeautifulSoup(response.data, 'html.parser')
+    h2 = postSoup.find('h2')
+    #assert the response has no messages
+    assert "Welcome Traveler" not in h2.text
 
 @pytest.fixture()
 def runner(app):
