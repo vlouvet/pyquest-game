c0c9a6830bb2405135f076fc4f89917dff980a32
vlouvet <louvetvicente@gmail.com>
2024-12-01 19:10:37 -0700

    remove incorrect test

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,18 +17,6 @@ def test_get_play(client):
     response = client.get("/")
     assert b"<h1>PyQuest Game</h1>" in response.data
 
-def test_post_play(client):
-    # Get the CSRF token
-    response = client.get("/")
-    soup = BeautifulSoup(response.data, 'html.parser')
-    csrf_token = soup.find(id='csrf_token')['value']
-    # Include the CSRF token in the POST request
-    response = client.post("/", data={"username": "l2@v1.c", "csrf_token": csrf_token})
-    postSoup = BeautifulSoup(response.data, 'html.parser')
-    h2 = postSoup.find('h2')
-    #assert the response has no messages
-    assert "Welcome Traveler" not in h2.text
-
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
